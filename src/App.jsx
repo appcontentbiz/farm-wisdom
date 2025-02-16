d2503bcf5f225a7fca61566a8b3c7c2b1fa64251
Your Name <you@example.com>
2025-02-15 22:03:42 -0600

    Fixed component imports to use correct page names

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import BeginnerGuide from './pages/BeginnerGuide';
 import Resources from './pages/Resources';
-import StateResources from './pages/StateResources';
-import Bloopers from './pages/Bloopers';
-import GleaningPrograms from './pages/GleaningPrograms';
-import BeginnerFarmer from './pages/BeginnerFarmer';
+import StateResourcesPage from './pages/StateResourcesPage';
+import BloopersPage from './pages/BloopersPage';
+import GleaningProgramsPage from './pages/GleaningProgramsPage';
+import BeginnerFarmerPage from './pages/BeginnerFarmerPage';
 import SmartCropPlanning from './pages/SmartCropPlanning';
 import PrecisionWeather from './pages/PrecisionWeather';
 import MarketIntelligence from './pages/MarketIntelligence';
 import ResourceOptimization from './pages/ResourceOptimization';
 import EquipmentManagement from './pages/EquipmentManagement';
 import CommunityHub from './pages/CommunityHub';
-import FarmDesigns from './pages/FarmDesigns';
-import TryDemo from './pages/TryDemo';
+import FarmDesignsPage from './pages/FarmDesignsPage';
+import Demo from './pages/Demo';
 import ResourceDirectory from './pages/ResourceDirectory';
 import Weather from './pages/Weather';
 import MarketPrices from './pages/MarketPrices';
@@ -42,23 +42,23 @@ function App() {
                   <Route path="/" element={<Home />} />
                   <Route path="/beginner-guide" element={<BeginnerGuide />} />
                   <Route path="/resources" element={<Resources />} />
-                  <Route path="/state-resources" element={<StateResources />} />
-                  <Route path="/bloopers" element={<Bloopers />} />
-                  <Route path="/gleaning-programs" element={<GleaningPrograms />} />
-                  <Route path="/beginner-farmer" element={<BeginnerFarmer />} />
+                  <Route path="/state-resources" element={<StateResourcesPage />} />
+                  <Route path="/bloopers" element={<BloopersPage />} />
+                  <Route path="/gleaning-programs" element={<GleaningProgramsPage />} />
+                  <Route path="/beginner-farmer" element={<BeginnerFarmerPage />} />
                   <Route path="/smart-crop-planning" element={<SmartCropPlanning />} />
                   <Route path="/precision-weather" element={<PrecisionWeather />} />
                   <Route path="/market-intelligence" element={<MarketIntelligence />} />
                   <Route path="/resource-optimization" element={<ResourceOptimization />} />
                   <Route path="/equipment-management" element={<EquipmentManagement />} />
                   <Route path="/community-hub" element={<CommunityHub />} />
-                  <Route path="/farm-designs" element={<FarmDesigns />} />
-                  <Route path="/try-demo" element={<TryDemo />} />
+                  <Route path="/farm-designs" element={<FarmDesignsPage />} />
+                  <Route path="/try-demo" element={<Demo />} />
                   <Route path="/resource-directory" element={<ResourceDirectory />} />
                   <Route path="/weather" element={<Weather />} />
                   <Route path="/market-prices" element={<MarketPrices />} />
                   <Route path="/seasonal-events" element={<SeasonalEvents />} />
-                  <Route path="/upgrade" element={<TryDemo />} />
+                  <Route path="/upgrade" element={<Demo />} />
                 </Routes>
               </main>
             </div>
